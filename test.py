1890dbcc6ef7bb0820054d14af81d47a186bf770
Joseph <joseph.espiritu@angel.com>
2022-08-31 14:31:21 -0600

    Team Generator

---
+++ test.py
@@ -0,0 +1,18 @@
+import random
+
+num_teams = int(input("How many teams? : "))
+with open('class_list.txt', "r") as readfile: 
+    people = readfile.readlines()
+
+num_people = len(people)
+
+while num_people > 0 and num_teams > 0:
+        
+    team = random.sample(people, int(num_people/num_teams))
+
+    for x in team:
+        people.remove(x)
+    
+    num_people-=int(num_people/num_teams)
+    num_teams-=1
+    print(team)
